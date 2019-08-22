f81688ecc58a362122bf63d36963720a08e1b6a3
ass3oooo <ass3oooo@gmail.com>
2019-08-22 17:23:49 +0300

    removed unused styles

--- assets/src/js/my.js
+++ assets/src/js/my.js
@@ -2,17 +2,15 @@
 
 // slider
 (function() {
-  let slider = $("#slider");
+  const BREAK_POINT = 575.98;
 
+  let slider = $("#slider");
   let sliderItems = slider.find("li.hero__item");
-  let sliderLabels = slider.find("li > .hero__item-label");
-  let sliderContents = slider.find("li > .hero__item-content");
 
   slider.on("click", (evt) => {
     let target = evt.target;
 
-    if (document.body.clientWidth > 575.98 && target.classList.contains("hero__item-label")) {
-      console.log("sliderLabel triggered");
+    if (document.body.clientWidth > BREAK_POINT && target.classList.contains("hero__item-label")) {
       sliderItems.removeClass("active");
       $(target).parent("li.hero__item").addClass("active");
     }
